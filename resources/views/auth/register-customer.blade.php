1a4805d5e8e79144872e0cad1afd29fd62a0484a
Wayne-Da-Creator <melia.joshwayne@ncst.edu.ph>
2026-07-30 06:21:36 +0800

    Testing3 (This is for local deploy script, nothing to worry about.)

--- resources/views/auth/register-customer.blade.php
+++ resources/views/auth/register-customer.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout title="Register Mo">
+<x-guest-layout title="Register">
     <div class="grid grid-cols-1 overflow-hidden lg:grid-cols-2 rounded-2xl">
 
         <!-- LEFT IMAGE PANEL -->
